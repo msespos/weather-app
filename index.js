230e9c5fe1a88f10a6ce462325373b5f912a6ee7
Mike Esposito <mikeesposito6852@gmail.com>
2022-11-26 18:34:24 -0800

    Set up basic fetching weather data and create object from that data

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-document.getElementById("test").append(document.createTextNode("Testing JS."));
+    async function fetchWeather() {
+      try {
+        const city = "Lomita";
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2fa07d454345910bbe52c05e16f97674",
+              {mode: 'cors'}
+             )
+        const allWeatherData = await response.json();
+        const weatherData = {
+          name: allWeatherData.name,
+          description: allWeatherData.weather[0].description
+        }
+        console.log("allWeatherData:", allWeatherData);
+        console.log("weatherData", weatherData);
+      } catch(error) {
+        console.log("Error; try again please.")
+      }
+    }
+    fetchWeather();
