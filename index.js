4e61def0d684169c75c4c5c75a17ea44208dedd2
Mike Esposito <mikeesposito6852@gmail.com>
2022-11-28 11:43:56 -0800

    Add temperature format option to form and displayed weather

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function fetchAndRenderWeather() {
     clearRenderSpace(p);
     p.appendChild(errorMessage);
   }
-}
+};
 const btn = document.getElementById("btn");
 btn.addEventListener("click", fetchAndRenderWeather);
 
@@ -16,7 +16,18 @@ async function fetchWeather() {
   const city = document.getElementById("city").value;
   const state = document.getElementById("state").value;
   const country = document.getElementById("country").value;
-  const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country + "&APPID=2fa07d454345910bbe52c05e16f97674", {mode: 'cors'})
+  let selectedTemperatureFormat = "f";
+  let selectedTemperatureFormatString = "&units=imperial";
+  const temperatureFormats = document.getElementsByName('temperature-format');
+  temperatureFormats.forEach((temperatureFormat) => {
+    if (temperatureFormat.checked) {
+      selectedTemperatureFormat = temperatureFormat.value;
+    }
+  });
+  if (selectedTemperatureFormat === "c") {
+    selectedTemperatureFormatString = "&units=metric";
+  }
+  const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "," + country + "&APPID=2fa07d454345910bbe52c05e16f97674" + selectedTemperatureFormatString, {mode: 'cors'})
   const allData = await response.json();
   const selectedWeatherData = {
     latitude: allData.coord.lat,
@@ -24,10 +35,13 @@ async function fetchWeather() {
     summary: allData.weather[0].main,
     details: allData.weather[0].description,
     temperature: allData.main.temp,
-    feelsLike: allData.main.feels_like
+    feelsLike: allData.main.feels_like,
+    temperatureFormat: selectedTemperatureFormat
   }
   return selectedWeatherData;
-}
+};
+
+// START HERE AND CLEAN UP THE PRESENTATION OF THE PROPERTIES (FAHRENHEIT NOT KELVIN E.G.)
 
 const renderWeather = (selectedWeatherData) => {
   const p = document.getElementById("p");
@@ -35,8 +49,16 @@ const renderWeather = (selectedWeatherData) => {
   const longitude = document.createTextNode("Longitude: " + selectedWeatherData.longitude);
   const summary = document.createTextNode("Summary: " + selectedWeatherData.summary);
   const details = document.createTextNode("Details: " + selectedWeatherData.details);
-  const temperature = document.createTextNode("Temperature now: " + selectedWeatherData.temperature);
-  const feelsLike = document.createTextNode("Feels Like: " + selectedWeatherData.feelsLike);
+  let temperature;
+  let feelsLike;
+  if (selectedWeatherData.temperatureFormat === "f") {
+    temperature = document.createTextNode("Temperature now: " + selectedWeatherData.temperature + "°F");
+    feelsLike = document.createTextNode("Feels like: " + selectedWeatherData.feelsLike + "°F");
+  } else {
+    temperature = document.createTextNode("Temperature now: " + selectedWeatherData.temperature + "°C");
+    feelsLike = document.createTextNode("Feels like: " + selectedWeatherData.feelsLike + "°C");
+  }
+  console.log(temperature);
   const properties = [latitude, longitude, summary, details, temperature, feelsLike];
   clearRenderSpace(p);
   properties.forEach((property) => {
@@ -44,10 +66,10 @@ const renderWeather = (selectedWeatherData) => {
     linebreak = document.createElement("br");
     p.appendChild(linebreak);
   });
-}
+};
 
 const clearRenderSpace = (p) => {
   while (p.firstChild) {
     p.removeChild(p.firstChild);
   }
-}
+};
