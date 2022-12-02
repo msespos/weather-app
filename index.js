fbaf7d18f5c33783ddd7765dc525b8e4e523c9cb
Mike Esposito <mikeesposito6852@gmail.com>
2022-12-01 19:27:51 -0800

    Add search for weather for selected city and display of weather summary

--- index.js
+++ index.js
@@ -36,12 +36,57 @@ const renderCityData = (city, cityData) => {
   const searchResults = document.getElementById("search-results");
   clear(searchResults);
   cityData.forEach((array) => {
+    const btn = document.createElement("button");
+    btn.textContent = "Select";
+    btn.onclick = () => {
+      console.log(array[0]);
+      fetchAndRenderWeatherData(array[0]);
+    };
+    searchResults.appendChild(btn);
     const cityAreaCountry = document.createTextNode(capitalizeFirstLetters(city) + ", " + array[1] + ", " + array[2]);
     searchResults.appendChild(cityAreaCountry);
     linebreak = document.createElement("br");
     searchResults.appendChild(linebreak);
   });
 };
+
+async function fetchAndRenderWeatherData(locationKey) {
+  try {
+    const weatherData = await fetchWeatherData(locationKey);
+    renderWeatherData(weatherData);
+  } catch(error) {
+    const p = document.getElementById("p");
+    const errorMessage = document.createTextNode("Error; try again please.");
+    clear(p);
+    p.appendChild(errorMessage);
+  }
+};
+
+async function fetchWeatherData(locationKey) {
+  const response = await fetch("http://dataservice.accuweather.com/currentconditions/v1/" + locationKey + "?apikey=SlhOykseiy5qmrZym7dGGVNRtiGU4Vh0&details=true", {mode: 'cors'})
+  const allWeatherData = await response.json();
+  const selectedWeatherData = {
+    summary: allWeatherData[0].WeatherText,
+    tempInF: allWeatherData[0].Temperature.Imperial.Value,
+    tempInC: allWeatherData[0].Temperature.Metric.Value,
+    humidity: allWeatherData[0].RelativeHumidity,
+    hasPrecipitation: allWeatherData[0].HasPrecipitation,
+    isDayTime: allWeatherData[0].IsDayTime,
+    icon: allWeatherData[0].WeatherIcon
+  }
+  return selectedWeatherData;
+}
+
+const renderWeatherData = (weatherData) => {
+  const p = document.getElementById("p");
+  clear(p);
+  const searchResults = document.getElementById("search-results");
+  clear(searchResults);
+  const weatherResults = document.getElementById("weather-results");
+  clear(weatherResults);
+  const summary = document.createTextNode("Summary: " + weatherData.summary);
+  weatherResults.appendChild(summary);
+}
 
 const clear = (e) => {
   while (e.firstChild) {
